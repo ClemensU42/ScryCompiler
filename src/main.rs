6aa48d3ad4dd84414d8ab0e2b2827aed1dea0f88
clemens <clemens_u@web.de>
2024-06-18 08:50:35 +0200

    added llvm17-0-prefer-static feature added to inkwell and added -i cli option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let mut input_file: String = String::new();
+    for i in 0..std::env::args().len(){
+        match std::env::args().nth(i).unwrap().as_str(){
+            "-i" => {input_file = std::env::args().nth(i + 1).unwrap()}
+            _ => {}
+        }
+    }
+
+    if input_file.is_empty() { return Err(Box::from("no input file specified!")); }
+    let input_file_content: String = fs::read_to_string(input_file)?;
+    println!("{input_file_content}");
+    Ok(())
 }
