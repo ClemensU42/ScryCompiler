2effc0e86737ba76a745a3ef7fb6d53f54d004ec
clemens <clemens_u@web.de>
2024-06-18 11:18:06 +0200

    added TokenType, Token and Lexer structs

---
+++ src/lexer.rs
@@ -0,0 +1,19 @@
+
+pub enum TokenType{
+
+}
+
+pub struct Token{
+    content: String,
+    token_type: TokenType
+}
+
+pub struct Lexer{
+    tokens: Vec<Token>
+}
+
+impl Lexer{
+    pub fn new(file: String){
+
+    }
+}
